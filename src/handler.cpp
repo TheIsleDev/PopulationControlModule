008c3b82ec783e57a07fddac831e7824ca9d83e2
BlackCrystalic <39885003+BlackCrystalic@users.noreply.github.com>
2026-07-07 00:44:22 +0500

    first stage of reflection

--- src/handler.cpp
+++ src/handler.cpp
@@ -9,6 +9,8 @@
 
 #include "Structs/TheIsleStructs.hpp"
 
+#include <Reflection/_include_custom.hpp>
+
 #include "_structs.hpp"
 
 namespace PopulationControlComponent {
@@ -28,8 +30,6 @@ namespace PopulationControlComponent {
 	static FProperty* _DinoClassGrowth{};
 	static FProperty* _DinoSteamId{};
 
-	static UFunction* _SetHealth{};
-
 	static UObject* GameMode{};
 
 	auto Fire(PopulationConfig::PopulationLimiterConfig LimiterConfig) -> void {
@@ -39,23 +39,23 @@ namespace PopulationControlComponent {
 		};
 
 		int TotalPlayersPlaying{0};// No, I know I can use Num, but this mod count lobby sitters too and ooman players
-		TMap<FString, TArray<IsleStructs::ATIDinosaurBase*>> AsocDinoArray;
-		TMap<FString, TArray<IsleStructs::ATIDinosaurBase*>> AsocDinoChildsArray;
+		TMap<FString, TArray<ATIDinosaurBase*>> AsocDinoArray;
+		TMap<FString, TArray<ATIDinosaurBase*>> AsocDinoChildsArray;
 
 		TArray<IsleStructs::FTIAvailableClassData>* CookedClasses = _GameModeCookedClasses->ContainerPtrToValuePtr<TArray<IsleStructs::FTIAvailableClassData>>(GameMode);
 		for (IsleStructs::FTIAvailableClassData& ClassData : *CookedClasses) {
 			FString DinoClassName = ClassData.Name.ToFString();
-			AsocDinoArray.Add(DinoClassName, TArray<IsleStructs::ATIDinosaurBase*>{});
-			AsocDinoChildsArray.Add(DinoClassName, TArray<IsleStructs::ATIDinosaurBase*>{});
+			AsocDinoArray.Add(DinoClassName, TArray<ATIDinosaurBase*>{});
+			AsocDinoChildsArray.Add(DinoClassName, TArray<ATIDinosaurBase*>{});
 		}
 
 		TArray<IsleStructs::ATIPlayerController*>* ActivePlayers = _GameModeAllControllers->ContainerPtrToValuePtr<TArray<IsleStructs::ATIPlayerController*>>(GameMode);
 		for (IsleStructs::ATIPlayerController* Player : *ActivePlayers) {
 			TotalPlayersPlaying++;
-			IsleStructs::APawn* Pawn = *_PlayerControllerPawn->ContainerPtrToValuePtr<IsleStructs::APawn*>(Player);
+			APawn* Pawn = *_PlayerControllerPawn->ContainerPtrToValuePtr<APawn*>(Player);
 			if (!Pawn || !Pawn->IsA(_DinoClass)) continue;// Make sure it's actually dino, not a fucking damn human
 
-			IsleStructs::ATIDinosaurBase* Dino = static_cast<IsleStructs::ATIDinosaurBase*>(Pawn);
+			ATIDinosaurBase* Dino = static_cast<ATIDinosaurBase*>(Pawn);
 			IsleStructs::FGeneralSettings GeneralSettings = *_DinoClassGeneralSettings->ContainerPtrToValuePtr<IsleStructs::FGeneralSettings>(Dino);
 			FString DinoClassName = GeneralSettings.ClassName.ToFString();
 			AsocDinoArray.Find(DinoClassName)->Add(Dino);// Not safe, may result in crash, there are edge case can be with dinoses that arent listed in CookedClasses in future
@@ -68,7 +68,7 @@ namespace PopulationControlComponent {
 		TSet<FString> ExcludedClasses;
 		for (PopulationConfig::SlotConfig& Slot : LimiterConfig.DinoClasses) {
 			FString DinoClassName = FString(to_wstring(Slot.name));
-			TArray<IsleStructs::ATIDinosaurBase*>* TargetArray = AsocDinoArray.Find(DinoClassName);
+			TArray<ATIDinosaurBase*>* TargetArray = AsocDinoArray.Find(DinoClassName);
 			if (!TargetArray) continue;// Safe check, just in case somebody mess up with config, if you set up it right there no chance for it to fail
 
 			int DinoNumber = TargetArray->Num();
@@ -88,23 +88,22 @@ namespace PopulationControlComponent {
 				continue;
 			}
 
-			TArray<IsleStructs::ATIDinosaurBase*> TargetArray = *AsocDinoChildsArray.Find(DinoClassName);// Here we for sure know this array exist, so no safe checks
+			TArray<ATIDinosaurBase*> TargetArray = *AsocDinoChildsArray.Find(DinoClassName);// Here we for sure know this array exist, so no safe checks
 			if (!TargetArray.Num()) continue;
 
-			IsleStructs::ATIDinosaurBase* Dino = TargetArray.Top();
+			ATIDinosaurBase* Dino = TargetArray.Top();
 			uint8 ClutchSize = *_DinoClassEggClutchSize->ContainerPtrToValuePtr<uint8>(Dino);// This way ppl can nest over limit
 			if (ClutchSize >= TargetArray.Num()) continue;
 
 			float HighestGrow = 1;
-			for (IsleStructs::ATIDinosaurBase* CurrentDino : TargetArray) {// Find the smallest one, so we wont be doing circle around killing the biggest one after somebody joins in
+			for (ATIDinosaurBase* CurrentDino : TargetArray) {// Find the smallest one, so we wont be doing circle around killing the biggest one after somebody joins in
 				float DinoGrowthPercent = *_DinoClassGrowth->ContainerPtrToValuePtr<float>(CurrentDino);
 				if (DinoGrowthPercent > HighestGrow) continue;
 				Dino = CurrentDino;
 				HighestGrow = DinoGrowthPercent;
 			}
 
-			IsleStructs::FSetHealthParams SetHealthParams{0};
-			Dino->ProcessEvent(_SetHealth, &SetHealthParams);
+			Dino->SetHealth(0);
 		}
 	}
 
@@ -122,7 +121,5 @@ namespace PopulationControlComponent {
 		_DinoClassEggClutchSize = _DinoClass->GetPropertyByNameInChain(STR("EggClutchSize"));
 		_DinoClassGrowth = _DinoClass->GetPropertyByNameInChain(STR("Growth"));
 		_DinoSteamId = _DinoClass->GetPropertyByNameInChain(STR("SteamId"));
-
-		_SetHealth = UObjectGlobals::StaticFindObject<UFunction*>(nullptr, nullptr, STR("/Script/TheIsle.TICharacterBase:SetHealth"));// Safe way to kill it
 	}
 }
